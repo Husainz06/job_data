095748ba08e2ede64ab238adfccc13c03847dd10
Husainz06 <aljaferh@msu.edu>
2024-10-12 13:24:45 -0400

    Update 2_About_Data.py

--- pages/2_About_Data.py
+++ pages/2_About_Data.py
@@ -9,7 +9,8 @@ original_df = pd.read_csv('/Users/husainz/Desktop/F2024/CMSE 830/Project/job_dat
 st.write(original_df.head(50))
 st.write('The data had to be cleaned in several ways to get to be usable for this project.\
          To read more details about the cleaning and encoding process, check out the \'Data Preparation\' tab.')
-df = pd.read_csv('/Users/husainz/Desktop/F2024/CMSE 830/Project/job_data/pages/cleaned_job_data.csv')
+original_df = pd.read_csv('/workspaces/job_data/pages/cleaned.csv')
+
 st.write('Data Sample:')
 st.write(df.head(50))
 
