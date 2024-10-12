baac48860b57350a7f8b3314a158e7a9d94d7ec6
Husainz06 <aljaferh@msu.edu>
2024-10-12 13:29:53 -0400

    Update 2_About_Data.py

--- pages/2_About_Data.py
+++ pages/2_About_Data.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.subheader('Data Overview')
 st.write("The dataset contsains a little over 12,300 Data Science job postings from\
          Indeed.com. Below is a sample of the original rawn data:\n")
-original_df = pd.read_csv('/Users/husainz/Desktop/F2024/CMSE 830/Project/job_data/pages/US_data_science_jobs_2023-6-19_0.csv')
+original_df = pd.read_csv('https://drive.google.com/file/d/1aRyAp1B3skvbNT1Q6x0hN9y4UFFok-yk/view?usp=share_link')
 st.write(original_df.head(50))
 st.write('The data had to be cleaned in several ways to get to be usable for this project.\
          To read more details about the cleaning and encoding process, check out the \'Data Preparation\' tab.')
