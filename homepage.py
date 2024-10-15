f3651fe63ca24734604d761b78078ef51446e8d4
Husainz06 <aljaferh@msu.edu>
2024-10-14 20:39:21 -0400

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,6 +1,22 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
-st.title('Data Science Job Analisys')
-st.write('Intro Page')
+
+st.title('About This Project')
+st.subheader('Story')
+st.image("pages/gol.jpeg", caption="The Game of Life")
+st.write('Have you ever played the \'The Game of Life\'?  Well if you have not, then here\'s a brief overview.\
+         The goal of the game is to make the most wealth when you retire. Your first decision in the game is\
+         to choose between getting a college degree or just start playing and look for a job. Everytime you reach \
+         A career point, you will be faced with three jobs to choose from. Some of them will require haveing a\
+         College degree while others will not. You probably have guessed it by now, yes jobs that require a college\
+         degree pay much better. \nThroughout the game, you will make important decisions that will affect your \
+         \'life\' such as getting married, investing some money, or choosing another career. \n')
+st.write("As a college student, you need to make some decisions that may affect your life on the long run.\
+         In this project, we try to analyze what effect learning some programming languages and/or technologies\
+         during your degree on your long term career. Even if you are not a college student, don't you want \
+         to know whether it is worth spending time and'/or money learning a language or a technology? Will that\
+         land you the job you're looking for? How much will you be paid? How many employers are looking for these\
+         qualifications?\n")
+st.write("In this application, we will try to help you decide whether you need to put in the effort or not. To do so\
+         we will be using real job posting data and analyze the data to get an idea of the job market requirements\
+        , employers, needed qualifications, salaries ...etc.\
+         To get more information about the data used in this application, chechout the 'About Data' tab.")
