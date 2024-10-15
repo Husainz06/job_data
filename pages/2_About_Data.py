013bae2655f2ee710b6c69cf567c8a0fa452409b
Husainz06 <aljaferh@msu.edu>
2024-10-14 20:42:18 -0400

    Update 2_About_Data.py
    Adding more text and removing the last part

--- pages/2_About_Data.py
+++ pages/2_About_Data.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-
-st.subheader('Data Overview')
-st.write("The dataset contsains a little over 12,300 Data Science job postings from\
-         Indeed.com. Below is a sample of the original rawn data:\n")
+st.title("Data Overview")
+st.subheader('Raw Data')
+st.write("This dataset began as raw data scraped from 'indeed.com'.")
+st.write("The dataset contsains a little over 12,300 Data Science job postings for \
+         Data Science jobs of all experience levels. Below is a sample of the original \
+         raw data that show the first 50 rows:\n")
 original_df = pd.read_csv('pages/originalOverview.csv')
 st.write(original_df.head(50))
-st.write('The data had to be cleaned in several ways to get to be usable for this project.\
-         To read more details about the cleaning and encoding process, check out the \'Data Preparation\' tab.')
-df = pd.read_csv('pages/cleaned.csv')
 
-st.write('Data Sample:')
+st.subheader('Cleaned Data Sample:')
+st.write('The data had to be cleaned in several ways to get it to be usable for this project.\
+         To read more details about the cleaning and encoding process, check out the\
+          \'Data Preparation\' tab. Below is a sample of the cleaned data. Note that the columns\
+         after \'Salary To\' change depending on the searched data.')
+df = pd.read_csv('pages/cleaned.csv')
 st.write(df.head(50))
 
 
 
 st.subheader("Basic Statistics")
+st.write('Below is some basic statistics of the cleaned sample above. This just shows some\
+         basic things like minimum value, maximum value, average, and standard deviation.')
 st.write(df.describe())
-
-st.subheader("Job count per state:")
-st.write(df.groupby('Location').size().reset_index(name='Job Count'))
