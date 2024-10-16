55014a0a029e18fc5ad667c63cf790ca004f67d7
Husainz06 <aljaferh@msu.edu>
2024-10-16 15:20:23 -0400

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Data Science Job Postings Analysis')
-st.subheader('Story')
+st.subheader('Let\'s Paint a Story')
 st.image("pages/gol.jpeg", caption="The Game of Life")
 st.write('Have you ever played the \'The Game of Life\'?  Well if you have not, then here\'s a brief overview.\
          The goal of the game is to make the most wealth when you retire. Your first decision in the game is\
@@ -9,8 +9,9 @@ st.write('Have you ever played the \'The Game of Life\'?  Well if you have not, 
          A career point, you will be faced with three jobs to choose from. Some of them will require haveing a\
          College degree while others will not. You probably have guessed it by now, yes jobs that require a college\
          degree pay much better. \nThroughout the game, you will make important decisions that will affect your \
-         \'life\' such as getting married, investing some money, or choosing another career. \n')
-st.write("As a college student, you need to make some decisions that may affect your life on the long run.\
+         \'life\' such as getting married, investing some money, or choosing another career. Looking at this\
+         one might think, isn\'t or life just that? Making decisions that will affect how our life goes?\n')
+st.write("As a college student, you might need to make some decisions that may affect your life on the long run.\
          In this project, we try to analyze what effect learning some programming languages and/or technologies\
          during your degree on your long term career. Even if you are not a college student, don't you want \
          to know whether it is worth spending time and'/or money learning a language or a technology? Will that\
@@ -18,5 +19,5 @@ st.write("As a college student, you need to make some decisions that may affect 
          qualifications?\n")
 st.write("In this application, we will try to help you decide whether you need to put in the effort or not. To do so\
          we will be using real job posting data and analyze the data to get an idea of the job market requirements\
-        , employers, needed qualifications, salaries ...etc.\
+        , employers, needed qualifications, salaries ...etc. Checkout \'Explore Jobs Data\' tab for more info. \
          To get more information about the data used in this application, chechout the 'About Data' tab.")
