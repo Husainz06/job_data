ccb47186467702017c4474a5b446208af29fd99e
Husainz06 <aljaferh@msu.edu>
2024-10-17 20:50:15 -0400

    Update 4_Imputation_Comparison.py
    Adding statistical comparison

--- pages/4_Imputation_Comparison.py
+++ pages/4_Imputation_Comparison.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from sklearn.impute import SimpleImputer
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.impute import KNNImputer
+
+
+original = pd.read_csv('pages/cleaned_v2.csv')
 
 st.title('Handling Missing Data - Imputation')
 st.write('The dataset has missing salary information and the data is missing \
@@ -9,9 +17,6 @@ st.write('The dataset has missing salary information and the data is missing \
          Data Imputation Techniques to see how the dataset is affected. First, let\'s see \
          the missing data that needs to be imputed.')
 
-# Create a heatmap to visualize missing data
-# Note that these are images as generating the plots slows down the loading
-# All codes are available on the GitHub Repo.
 st.image("Images/missing_heat_map.png", caption="Missing Data")
 
 st.write('As can be seen from the figure above, there\'s a lot of missing salary data \
@@ -43,14 +48,42 @@ st.write('Looking at the violin plot, we can see that there\'s a good amount of 
          when we combine the information we can get from the histograms above, the box plots, and the \
          violin plots, we can clearly see that KNN imputation performed much better in this particular \
          case.')
-st.subheader("Statistical Comparison")
-st.write("Add statistical comparison between the original dataset and methods here")
+st.subheader('Statistical Comparison')
+st.write("Let's do check some basic statistics on the datasets to help us come to \
+         a conclusion.")
+original = pd.read_csv('pages/cleaned_v2.csv')
+mean_imputed = pd.read_csv('pages/mean_imputed_v2.csv')
+knn_imputed = pd.read_csv('pages/knn_imputed_v2.csv')
+
+st.table(original.describe())
+st.markdown("<h5 style='text-align: center;'>Original Data Before Imputation</h5>", unsafe_allow_html=True)
+
+st.caption("Original Data Without Imputation")
+st.table(mean_imputed.describe())
+st.markdown("<h5 style='text-align: center;'>Mean Imputed Data</h5>", unsafe_allow_html=True)
+
+st.table(knn_imputed.describe())
+st.markdown("<h5 style='text-align: center;'>KNN Imputed Data</h5>", unsafe_allow_html=True)
+st.write("Looking at the tables above we can first see the large amount of missing salary \
+         data. This raises a good question that is 'How effective and accurate an \
+         imputation method is going to be? and do we have enough data to get good \
+         results?'")
+st.write("Comparing the minimum and maximum salaries in the imputed datasets and \
+         the original, we ca see that in all three datasets the maximums are the \
+         same. However, we can see the problem in the mean imputed dataset which \
+         is the negative salary values. This confirms to us that we can not reliably use \
+         mean imputation here.")
 st.subheader('Thoughts and Conclusion')
 st.write('While KNN imputation performed better than mean imputation in this applcation, there might be \
          more to explore here. Other imputation techniques might yeild better results. There could be some \
          useful correlations that can be used to impute salaries here such as the location of the job, \
-         the experience level and the required qualifications.')
-st.write("We will need to do more testing using other imputation methods that take these factors into \
+         the experience level and the required qualifications. Some questions remain unasnwered here \
+         such as \'Will another imputation method be more reliable? and how to test that?\' \'Is it \
+         still a vaibale solution to imput while having this much missing data?\' \'Is there a better \
+         solution? or should we just use the original dataset with missingness?\' Some of these \
+         qustions are subjective but we need to come up with a way to quantify the results to better \
+         answer them.')
+st.write("We feel that will need to do more testing using other imputation methods that more factors  and features into \
          consideration but in the meantime, we will be using the KNN imputed dataset for the rest of this \
          application and may compare it sometimes to the original un-imputed dataset. The reson for this \
          comparison is because the purpose of the application is to help you make a decision that could be \
