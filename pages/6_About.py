92fda95bcd906d20064c336d1feafbea6a6ef1ac
Husainz06 <aljaferh@msu.edu>
2024-10-18 16:49:22 -0400

    Update 6_About.py

--- pages/6_About.py
+++ pages/6_About.py
@@ -1,2 +1,14 @@
-import streamlit as st
+import stramlit as st
+
 st.title('About This Application')
+st.subheader('Purpose')
+st.write('This application was implemented by Hussain A. Aljafer as a part of the Foundations of \
+         Data Science class (CMSE 830) offered at Michigan State University.')
+st.subheader('Motivation')
+st.write("The motivation of this application is my passion of programming and data analysis and the \
+         excitement to come up with something that uses real life data and can be useful even beyound \
+         the scope of a coursework. This application is intended to help future or even data scientist \
+         in making a decision on what technologies or qualifications they may need to land a good job.")
+st.subheader('Version')
+st.write('Since you see this part, then the appliation is not yet complete as this is the first version \
+         of the application. The application will be improved and more featuress will be added soon.')
